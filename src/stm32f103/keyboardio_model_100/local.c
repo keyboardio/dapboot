1e3ffac548c164aee345d0e1773c87a216952849
Jesse Vincent <jesse@keyboard.io>
2022-01-10 19:05:00 -0800

    Make the bootloader unwilling to start up unless you're holding the safety interlock key

--- src/stm32f103/keyboardio_model_100/local.c
+++ src/stm32f103/keyboardio_model_100/local.c
@@ -1,5 +1,16 @@
+#include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include "target.h"
+#include "config.h"
+#include "backup.h"
+
+#ifndef REG_BOOT
+#define REG_BOOT BKP1
+#endif
+
+#ifndef CMD_BOOT
+#define CMD_BOOT 0x4F42UL
+#endif
 
 
 void target_pre_main(void) {
@@ -14,5 +25,41 @@ void target_pre_main(void) {
     // disable all RCC interrupts
     RCC_CIR   = 0x00000000;
 
+}
+
+bool target_get_force_bootloader(void) {
+    bool force = false;
+
+    /* We don't check the RTC backup register. 
+     * If the reboot happened, we only care if 
+     * they're holding down the 'program' button 
+     */
+    // uint16_t cmd = backup_read(REG_BOOT);
+    // if (cmd == CMD_BOOT) {
+    //    force = true;
+    // }
+
+    /* Clear the RTC backup register */
+    backup_write(REG_BOOT, 0);
+
+#if HAVE_BUTTON
+    /* Wait some time in case the button has some debounce capacitor */
+    int i;
+    for (i = 0; i < BUTTON_SAMPLE_DELAY_CYCLES; i++) {
+        __asm__("nop");
+    }
+    /* Check if the user button is held down */
+    if (BUTTON_ACTIVE_HIGH) {
+        if (gpio_get(BUTTON_GPIO_PORT, BUTTON_GPIO_PIN)) {
+            force = true;
+        }
+    } else {
+        if (!gpio_get(BUTTON_GPIO_PORT, BUTTON_GPIO_PIN)) {
+            force = true;
+        }
+    }
+#endif
+
+    return force;
 }
 
