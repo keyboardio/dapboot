696599eb51e7b81eac29a68bcc6e77548fff5446
Jesse Vincent <jesse@keyboard.io>
2022-03-11 18:34:08 -0800

    since we have a button, we don't need to ifdef it

--- src/stm32f103/keyboardio_model_100/local.c
+++ src/stm32f103/keyboardio_model_100/local.c
@@ -47,23 +47,15 @@ bool target_get_force_bootloader(void) {
     /* Clear the RTC backup register */
     //backup_write(REG_BOOT, 0);
 
-#if HAVE_BUTTON
     /* Wait some time in case the button has some debounce capacitor */
-    int i;
-    for (i = 0; i < BUTTON_SAMPLE_DELAY_CYCLES; i++) {
+    for (int i = 0; i < BUTTON_SAMPLE_DELAY_CYCLES; i++) {
         __asm__("nop");
     }
     /* Check if the user button is held down */
-    if (BUTTON_ACTIVE_HIGH) {
         if (gpio_get(BUTTON_GPIO_PORT, BUTTON_GPIO_PIN)) {
             force = true;
         }
-    } else {
-        if (!gpio_get(BUTTON_GPIO_PORT, BUTTON_GPIO_PIN)) {
-            force = true;
-        }
-    }
-#endif
+
 
     return force;
 }
